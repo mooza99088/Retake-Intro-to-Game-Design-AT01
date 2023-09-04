ef55063ad59a39a752099c99bd597f66a482e3e1
mooza <30060241@tafe.wa.edu.au>
2023-09-04 15:08:46 +0800

    13 Commit
    fixed bug in project where player walked through walls, added to document, created project schedule.

--- AT02 Gamification Project/Assets/Scripts/PlayerMovement.cs
+++ AT02 Gamification Project/Assets/Scripts/PlayerMovement.cs
@@ -5,40 +5,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private CharacterController characterController;
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        // Find the main camera in the scene and get its transform
+        cameraTransform = Camera.main.transform;
+    }
 
     void Update()
     {
         // Get input from the player
-        float horizontalInput = 0f;
-        float verticalInput = 0f;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            verticalInput = 1f;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            verticalInput = -1f;
-        }
+        // Calculate the movement direction relative to the camera
+        Vector3 moveDirection = cameraTransform.forward * verticalInput + cameraTransform.right * horizontalInput;
+        moveDirection.y = 0f; // Ensure the player stays grounded
 
-        if (Input.GetKey(KeyCode.A))
+        // Normalize the movement direction to prevent diagonal movement from being faster
+        if (moveDirection.magnitude > 1f)
         {
-            horizontalInput = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            horizontalInput = 1f;
+            moveDirection.Normalize();
         }
 
-        // Calculate the movement direction
-        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
-        // Move the player
+        // Move the player using Character Controller
         if (moveDirection.magnitude >= 0.1f)
         {
             // Apply movement speed and deltaTime for smooth movement
             Vector3 moveVector = moveDirection * moveSpeed * Time.deltaTime;
-            transform.Translate(moveVector, Space.Self);
+
+            // Rotate the player to match the camera's forward direction
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+
+            // Use the Character Controller methods for movement
+            characterController.Move(moveVector);
         }
     }
 }
+
+
